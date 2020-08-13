d311bcbe4ee46b38132a53fbfc932d6a1436e8b9
Rpx <ritaa.pinho@gmail.com>
2020-08-13 05:25:40 +0100

    fixed removed button give classes

--- public/scripts/field.js
+++ public/scripts/field.js
@@ -1,47 +1,43 @@
 // Clones Schedule Field
 
-function cloneField(){
-    const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true)
-    let fields = newFieldContainer.querySelectorAll('input')
-    fields.forEach(function(field){
-        field.value = ""
-    })
-    document.querySelector("#schedule-items").appendChild(newFieldContainer)
-  
+function cloneField() {
+  const newFieldContainer = document
+    .querySelector(".schedule-item")
+    .cloneNode(true);
+  console.log(newFieldContainer);
+  let fields = newFieldContainer.querySelectorAll("input");
+  fields.forEach(function (field) {
+    field.value = "";
+  });
+  document.querySelector("#schedule-items").appendChild(newFieldContainer);
+  const removeBtn = newFieldContainer.querySelector(".remove-schedule-item");
+  if (removeBtn) {
+    removeBtn.addEventListener("click", function () {
+      newFieldContainer.remove();
+    });
+  }
 }
-
 // Adds new field
 
-function addButton(){
-    let button = document.querySelector("#add-time")
-    const selected = document.getElementById('select').selected
-    console.log(selected)
-    let scheduleItems = document.querySelector('#schedule-items')
-    console.log(scheduleItems)
-    let inputs = scheduleItems.querySelectorAll('input')
-            if(selected == true || [...inputs].some(input=>input.value === "")){
-            alert('Tem o dia ou a hora faltando nos Horários Disponíveis.')
-            button.removeEventListener('click',cloneField)
-
-        }else{            
-            button.addEventListener('click',cloneField)
-    }   
-    }
+function addButton() {
+  let button = document.querySelector("#add-time");
+  const selected = document.getElementById("select").selected;
+  let scheduleItems = document.querySelector("#schedule-items");
+  let inputs = scheduleItems.querySelectorAll("input");
+  if (selected == true || [...inputs].some(input => input.value === "")) {
+    alert("Tem o dia ou a hora faltando nos Horários Disponíveis.");
+    button.removeEventListener("click", cloneField);
+  } else {
+    button.addEventListener("click", cloneField);
+  }
+}
 // Removed field added if needed
 
-let buttonRemove = document.querySelector('.remove-schedule-item')
-buttonRemove.addEventListener('click',removeField)
+let buttonRemove = document.querySelector(".remove-schedule-item");
+buttonRemove.addEventListener("click", removeField);
 
-function removeField(){
-    let scheduleItem = document.querySelector('.schedule-item')
-    scheduleItem.parentNode.removeChild(scheduleItem);
-
+function removeField() {
+  let scheduleItem = document.querySelector(".schedule-item");
+  scheduleItem.parentNode.removeChild(scheduleItem);
+  console.log("hey");
 }
-
-
-
-
-
-
-
-
