53cdbc8a67e39567cf4f6086ce50ade438f339dd
Rpx <ritaa.pinho@gmail.com>
2020-08-17 05:10:18 +0100

    added 'proffys' to teachers page

--- src/pages.js
+++ src/pages.js
@@ -1,94 +1,97 @@
-const Database = require ('./database/db')
+const Database = require("./database/db");
 
+const {
+  subjects,
+  weekdays,
+  getSubject,
+  convertHourstoMinutes,
+} = require("./utils/format");
 
-const { subjects,weekdays,getSubject,convertHourstoMinutes } = require('./utils/format')
-
-
-function pageLanding(req,res){
-    return res.render("index.html")
+function pageLanding(req, res) {
+  return res.render("index.html");
 }
 
-async function pageStudy(req,res){
+async function pageStudy(req, res) {
+  const filters = req.query;
 
+  if (!filters.subject || !filters.weekday) {
+    return res.render("study.html", { filters, subjects, weekdays });
+  }
 
-    const filters = req.query 
-
-
-    if (!filters.subject || !filters.weekday || !filters.time ){
-        return res.render("study.html",{filters,subjects,weekdays})
-    }
-
-    const timeToMinutes = convertHourstoMinutes(filters.time)
-
-    const query = `
+  const timeToMinutes = convertHourstoMinutes(filters.time);
+  let query = `
     SELECT classes.*, proffys.*
     FROM proffys
+    JOIN classes ON (classes.proffy_id = proffys.id)`;
+  if (filters.time) {
+    query = `SELECT classes.*, proffys.*
+    FROM proffys
     JOIN classes ON (classes.proffy_id = proffys.id)
-    WHERE EXISTS (
-        SELECT class_schedule.*
-        FROM class_schedule
-        WHERE class_schedule.class_id = classes.id
-        AND class_schedule.weekday = ${filters.weekday}
-        AND class_schedule.time_from <= ${timeToMinutes}
-        AND class_schedule.time_to > ${timeToMinutes}
-    )
-    AND classes.subject = '${filters.subject}'
-    `
-     try{
-         const db = await Database
-         const proffys = await db.all(query)
-         return res.render('study.html', {proffys, subjects, filters, weekdays })
-     }catch(error){
-         console.log(error)
-     } 
+      WHERE EXISTS (
+      SELECT class_schedule.*
+      FROM class_schedule
+      WHERE class_schedule.class_id = classes.id
+      AND class_schedule.weekday = ${filters.weekday}
+      AND class_schedule.time_from <= ${timeToMinutes}
+      AND class_schedule.time_to > ${timeToMinutes}
+  )
+  AND classes.subject = '${filters.subject}'`;
+  }
+
+  try {
+    const db = await Database;
+    const proffys = await db.all(query);
+    return res.render("study.html", { proffys, subjects, filters, weekdays });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function pageGiveClasses(req,res){
-    return res.render("give-classes.html",{subjects,weekdays})   
+function pageGiveClasses(req, res) {
+  return res.render("give-classes.html", { subjects, weekdays });
 }
 
-async function saveClasses(req,res){
-    const createProffy = require('./database/createProffy')
-    const proffyValue={
-        name:req.body.name,
-        avatar:req.body.avatar,
-        whatsapp:req.body.whatsapp,
-        bio: req.body.bio
-    }
-    const classValue = {
-        subject: req.body.subject,
-        cost:req.body.cost
-    }
-    const classScheduleValues = req.body.weekday.map((weekday,index) => {
-        return {
-            
-                weekday,
-                time_from: convertHourstoMinutes(req.body.time_from[index]),
-                time_to: convertHourstoMinutes(req.body.time_to[index])
-        }
-    })
+async function saveClasses(req, res) {
+  const createProffy = require("./database/createProffy");
+  const proffyValue = {
+    name: req.body.name,
+    avatar: req.body.avatar,
+    whatsapp: req.body.whatsapp,
+    bio: req.body.bio,
+  };
+  const classValue = {
+    subject: req.body.subject,
+    cost: req.body.cost,
+  };
+  const classScheduleValues = req.body.weekday.map((weekday, index) => {
+    return {
+      weekday,
+      time_from: convertHourstoMinutes(req.body.time_from[index]),
+      time_to: convertHourstoMinutes(req.body.time_to[index]),
+    };
+  });
 
-    try {
-        const db = await Database
-        await createProffy(db, {proffyValue,classValue,classScheduleValues})
-        let queryString = "?subject=" + req.body.subject
-        queryString += "&weekday=" + req.body.weekday[0]
-        queryString += "&time=" + req.body.time_from[0]
-        return res.redirect("/success" + queryString)
-    } catch(error){
-        console.log(error)
-    }   
+  try {
+    const db = await Database;
+    await createProffy(db, { proffyValue, classValue, classScheduleValues });
+    let queryString = "?subject=" + req.body.subject;
+    queryString += "&weekday=" + req.body.weekday[0];
+    queryString += "&time=" + req.body.time_from[0];
+    return res.redirect("/success" + queryString);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function pageSuccess(req,res){
-    return res.render("success.html")
+function pageSuccess(req, res) {
+  return res.render("success.html");
 }
 
 module.exports = {
-    pageLanding,
-    getSubject,
-    pageStudy,
-    pageGiveClasses,
-    saveClasses,
-    pageSuccess
-}
+  pageLanding,
+  getSubject,
+  pageStudy,
+  pageGiveClasses,
+  saveClasses,
+  pageSuccess,
+};
